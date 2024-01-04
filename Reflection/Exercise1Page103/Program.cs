a35dd0fad18eb6047912df47f1e067e282906c92
Zdravko Koldzhiev <92548558+zdravkokold@users.noreply.github.com>
2024-01-04 17:45:21 +0200

    Add files via upload

---
+++ Reflection/Exercise1Page103/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Exercise1Page103
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            var sb = new StringBuilder();
+
+            foreach (var type in types.Where(x => x.Namespace.StartsWith("Work")))
+            {
+                var typeInfo = type.GetTypeInfo();
+                sb.AppendLine(type.Name);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+
+    public class Worker
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; private set; }
+
+        public Worker(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Salary: ${Salary}");
+        }
+    }
+}
